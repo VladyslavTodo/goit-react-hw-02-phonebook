4dee1f9beae81f89f77d19ffef5ab0332e2caabe
Vladyslav <124445034+VladyslavTodo@users.noreply.github.com>
2023-10-22 16:53:30 +0300

    Update ContactsForm.jsx

--- src/components/ContactForm/ContactsForm.jsx
+++ src/components/ContactForm/ContactsForm.jsx
@@ -32,7 +32,7 @@ class ContacsForm extends Component {
           <input
             type="text"
             name="name"
-            pattern={^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$}
+            pattern={'^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$'}
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             value={name}
             onChange={this.handleChange}
@@ -44,7 +44,7 @@ class ContacsForm extends Component {
           <input
             type="tel"
             name="number"
-            pattern={\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}}
+            pattern={'\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}'}
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             value={number}
             onChange={this.handleChange}
